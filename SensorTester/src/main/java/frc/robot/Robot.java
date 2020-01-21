1baed8cbdbd085cc68f562261c97377a1f9d518f
AndyC <andrew.cappon@gmail.com>
2020-01-20 20:45:33 -0800

    SensorTester is RobotBase instead of TimedBase, which seems to work better for debugging

---
+++ SensorTester/src/main/java/frc/robot/Robot.java
@@ -0,0 +1,61 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot;
+
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.PWMVictorSPX;
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+import com.analog.adis16448.frc.ADIS16448_IMU;
+
+/**
+ * This is a demo program showing the use of the RobotDrive class, specifically
+ * it contains the code necessary to operate a robot with tank drive.
+ */
+public class Robot extends TimedRobot {
+  private DifferentialDrive m_myRobot;
+  private Joystick m_leftStick;
+  private Joystick m_rightStick;
+
+  private boolean m_runCal = false;
+  private boolean m_configCal = false;
+  private boolean m_reset = false;
+  private boolean m_setYawAxis = false;
+  public static ADIS16448_IMU m_imu = new ADIS16448_IMU();
+  public static VisualOdometer m_visOdometer = new VisualOdometer();
+
+  @Override
+  public void robotInit() {
+    m_myRobot = new DifferentialDrive(new PWMVictorSPX(0), new PWMVictorSPX(1));
+    m_leftStick = new Joystick(0);
+    m_rightStick = new Joystick(1);
+    
+    // Set IMU settings
+    if (m_configCal) {
+      m_imu.configCalTime(8);
+      m_configCal = SmartDashboard.putBoolean("ConfigCal", false);
+    }
+    if (m_reset) {
+      m_imu.reset();
+      m_reset = SmartDashboard.putBoolean("Reset", false);
+    }
+    if (m_runCal) {
+      m_imu.calibrate();
+      m_runCal = SmartDashboard.putBoolean("RunCal", false);
+    }
+}
+
+  @Override
+  public void teleopPeriodic() {
+    m_myRobot.tankDrive(m_leftStick.getY(), m_rightStick.getY());
+
+    m_visOdometer.update();
+  }
+}
